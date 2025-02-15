b32710774e762998c604eb22fcf5364fa3c5c826
Dhruv <dhruvvv099@gmail.com>
2025-02-15 18:15:42 +0530

    Fixed .gitignore and cleared cache

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -2,4 +2,15 @@ import express from "express";
 
 const router = express.Router()
 
+router.post("/signup", (req,res) =>{
+    res.send("signup route")
+});
+
+router.post("/login", (req,res) =>{
+    res.send("login route")
+});
+
+router.get("/logout", (req,res) =>{
+    res.send("logout route")
+});
 export default router;
