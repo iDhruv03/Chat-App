6f37a1fa086e71c55ff4d6ad7dd732bbb8c2945e
Dhruv <dhruvvv099@gmail.com>
2025-02-15 18:58:00 +0530

    commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,11 @@
 import express from "express";
+import dotenv from "dotenv";
 import authRoutes  from "./routes/auth.route.js";
 
+dotenv.config()
 const app = express();
-
+const PORT = process.env.PORT
 app.use("/api/auth", authRoutes);
-app.listen(5001, () =>{
-    console.log("server is running on port 5001");
+app.listen(PORT, () =>{
+    console.log("server is running on on PORT:" + PORT);
 });
