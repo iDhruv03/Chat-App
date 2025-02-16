99ca21873eb9351be8fff984f198ef3553724b4b
Dhruv <dhruvvv099@gmail.com>
2025-02-17 01:24:31 +0530

    Added sendMessage controller

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,3 +1,4 @@
+import cloudinary from "../lib/cloudinary.js";
 import Message from "../models/message.model.js";
 import User from "../models/user.model.js";
 
@@ -28,5 +29,33 @@ export const getMessages = async(req,res) =>{
     } catch (error) {
         console.log("Error in getMessages controller: ", error.message);
         res.status(500).json({ error: "Internal Server Error"});
+    }
+};
+
+export const sendMessage = async(req,res) =>{
+
+    try {
+        const{ text, image } = req.body;
+        const{ id: receiverId} = req.params;
+        const senderId = req.user._id;
+
+        let imageUrl;
+        if(image){ 
+            const uploadResponse = await cloudinary.uploader.upload(image);
+            imageUrl = uploadResponse.secure_url;
+        }
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            text,
+            image: imageUrl,
+        });
+
+        await newMessage.save();
+
+        res.status(201).json(newMessage);
+    } catch (error) {
+        console.log("Error in sendMessage controller: ", error.message);
+        res.status(500).json({ error: "Internal server error"});
     }
 };
