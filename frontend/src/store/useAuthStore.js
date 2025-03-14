5afce956073824f9c40de45c162248d9b14c5f5a
Dhruv <dhruvvv099@gmail.com>
2025-03-14 22:12:36 +0530

    checkAuth

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,9 +1,17 @@
-import {crate} from "zustand";
-
+import {create} from "zustand";
+import { axiosInstance } from "../lib/axios";
 export const useAuthStore = create((set) => ({
     authUser: null,
     isCheckingAuth: true,
     isSigningUp: false,
     isLoggingIn: false,
     isUpdatingProfile: false,
+
+    checkAuth: async() => {
+        try {
+            const res = await aciosInstance.get("/auth/check");
+        } catch (error) {
+            
+        }
+    }
 }));
