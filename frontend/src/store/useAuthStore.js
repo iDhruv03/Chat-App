595869200f4d33f23e63ffb012ee9c4fe88490a0
Dhruv <dhruvvv099@gmail.com>
2025-03-14 22:09:35 +0530

    authStore

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,5 +3,7 @@ import {crate} from "zustand";
 export const useAuthStore = create((set) => ({
     authUser: null,
     isCheckingAuth: true,
-    
-}))
+    isSigningUp: false,
+    isLoggingIn: false,
+    isUpdatingProfile: false,
+}));
