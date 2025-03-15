ade31354e4018a023412a8c33c25b6f2380a1eac
Dhruv <dhruvvv099@gmail.com>
2025-03-15 13:18:28 +0530

    signUpPage UI

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -18,5 +18,9 @@ export const useAuthStore = create((set) => ({
         } finally{
             set({ isCheckingAuth: false});
         }
+    },
+
+    signup: async (data) =>{
+        
     }
 }));
