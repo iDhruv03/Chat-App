5f823ec8167f77f02b4c9eabbe6f7b4627519be9
Dhruv <dhruvvv099@gmail.com>
2025-03-15 13:02:11 +0530

    Signup page structure

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const SignUpPage = () => {
-  return (
-    <div>SignUpPage</div>
-  )
+  const [showPassword, setShowPasword] = useState(false);
+  const [formData, setFormData] = useState({
+    fullName: "",
+    email: "",
+    password: "",
+  });
+
+  return  <div>SignUpPage</div>
+  
 }
 
 export default SignUpPage
