4d910daed4657da68e06db4388a39e94fe1b3023
Dhruv <dhruvvv099@gmail.com>
2025-03-22 13:34:38 +0530

    completed form validation

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -18,9 +18,16 @@ const SignUpPage = () => {
       if (!formData.email.trim()) return toast.error("Email is required");
       if (!/\S+@\S+\.\S+/.test(formData.email)) return toast.error("Invalid email format");
       if (!formData.password) return toast.error("Password is required");
+      if (formData.password.length < 6) return toast.error("Password must be at least 6 characters");
+
+      return true;
   };
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    const success = validateForm();
+
+    if(success===true) signup(formData);
   };
 
   return(  <div className="min-h-screen grid lg:grid-cols-2">
