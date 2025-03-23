557d61560b9675e89b90a0c8d2420693fdbc918d
Dhruv <dhruvvv099@gmail.com>
2025-03-23 18:23:11 +0530

    navbar init

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,11 +3,12 @@ import { useAuthStore } from '../store/useAuthStore';
 
 const Navbar = () => {
 
-      
+      const {logout, authUser } = useAuthStore();
     
-  return (
-    <div>Navbar</div>
-  )
+  return <header className="bg-base-100 border-b border-base-300 fixed w-full top-0 z-40 
+    backdrop-blur-lg bg-base-100/80">
+
+  </header>
 }
 
 export default Navbar;
