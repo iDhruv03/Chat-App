e2d0df4440cb0ac07ae08d442709407e3d5bc730
Dhruv <dhruvvv099@gmail.com>
2025-03-23 09:50:16 +0530

    signup completed

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -30,5 +30,8 @@ export const useAuthStore = create((set) => ({
         } catch (error) {
             toast.error(error.response.data.message);
         }
+        finally{
+            set({isSigningUp: false});
+        }
     }
 }));
