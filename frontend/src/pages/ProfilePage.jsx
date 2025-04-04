086dded4cbd97d90299977160ede1ffec447c098
Dhruv <dhruvvv099@gmail.com>
2025-04-04 23:43:28 +0530

    image upload skeleton

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 import { useAuthStore } from '../store/useAuthStore'
 
 const ProfilePage = () => {
-  
+  const {authUser, isUpdatingProfile, updateProfile} = useAuthStore();
+
+  const handleImageUpload = async(e) => {
+    
+  }
   return (
     <div>ProfilePage</div>
   )
