0dd95c2f0fbef1cb97b1a415aea0c9ac4bfecb1b
Dhruv <dhruvvv099@gmail.com>
2025-04-05 11:36:01 +0530

    added prof pic upload

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -6,8 +6,18 @@ const ProfilePage = () => {
   const {authUser, isUpdatingProfile, updateProfile} = useAuthStore();
   const [selectedImg, setSelectedImg] = useState(null);
   const handleImageUpload = async(e) => {
+    const file = e.target.files[0];
+    if(!file) return;
 
-  }
+    const reader = new FileReader();
+
+    reader.readAsDataURL(file);
+    reader.onload = async () => {
+      const base64Image = reader.result;
+      await updateProfile({ profilePic: base64Image });
+    }
+  };
+  
   return (
     <div className="h-screen pt-20">
       <div className="max-w-2xl mx-auto p-4 py-8">
