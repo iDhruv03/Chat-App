11981841fac6477710ad933f921e6aa615ce55ba
Dhruv <dhruvvv099@gmail.com>
2025-04-05 18:47:54 +0530

    new functions

--- frontend/src/components/MessageInput.jsx
+++ frontend/src/components/MessageInput.jsx
@@ -1,13 +1,26 @@
 import React, { useRef, useState } from 'react'
+import { useChatStore } from '../store/useChatStore';
 
 const MessageInput = () => {
   
   const [text, setText] = useState("");
   const [imagePreview, setImagePreview] = useState(null);
   const fileInputRef = useRef(null);
-  
+  const {sendMessage} = useChatStore();
+
+    const handleImageChange = (e) => {};
+
+    const removeImage = () => {};
+
+    const handleSendMessage = async(e) =>{};
+
+
+
+
   return (
-    <div>MessageInput</div>
+    <div className="p-4 w-full">
+
+    </div>
   )
 }
 
