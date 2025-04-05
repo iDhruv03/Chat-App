dc5e7b400812568c3baa31ff26e04842d63e3caa
Dhruv <dhruvvv099@gmail.com>
2025-04-05 11:40:57 +0530

    custom hook fix

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -5,6 +5,7 @@ import {Camera, Mail, User} from "lucide-react";
 const ProfilePage = () => {
   const {authUser, isUpdatingProfile, updateProfile} = useAuthStore();
   const [selectedImg, setSelectedImg] = useState(null);
+
   const handleImageUpload = async(e) => {
     const file = e.target.files[0];
     if(!file) return;
@@ -14,10 +15,11 @@ const ProfilePage = () => {
     reader.readAsDataURL(file);
     reader.onload = async () => {
       const base64Image = reader.result;
+      setSelectedImg(base64Image);
       await updateProfile({ profilePic: base64Image });
     }
   };
-  
+
   return (
     <div className="h-screen pt-20">
       <div className="max-w-2xl mx-auto p-4 py-8">
