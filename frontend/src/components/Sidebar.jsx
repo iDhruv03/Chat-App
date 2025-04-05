9d90b962cd4dcfe5b892d3b5d57082a8fc835bce
Dhruv <dhruvvv099@gmail.com>
2025-04-05 14:26:49 +0530

    sidebar update

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import { userChatStore } from '../store/useChatStore'
 import SidebarSkeleton from "./skeletons/SidebarSkeleton";
+import { Users } from 'lucide-react';
 
 const Sidebar = () => {
 
@@ -16,7 +17,13 @@ const Sidebar = () => {
 
   return (
     <aside className="h-full w-20 lg:w-72 border-r border-base-300 flex flex-col transition-all duration-200">
-
+        <div className="border-b border-base-300 w-full p-5">
+            <div className="flex items-center gap-2">
+                <Users className="size-6" />
+                <span className="font-medium hidden lg:block">Contacts</span>
+            </div>
+            {/* need to add online user toggle */}
+        </div>
     </aside>
   )
 }
