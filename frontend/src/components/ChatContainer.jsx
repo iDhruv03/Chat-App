1a9f84aacd3111aa51c239c37efab1f8cd2c78bf
Dhruv <dhruvvv099@gmail.com>
2025-04-06 10:09:33 +0530

    chat image implementation

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -26,7 +26,7 @@ const ChatContainer = () => {
       <ChatHeader />
       <div className="flex-1 overflow-y-auto p-4 space-y-4">
           {messages.map((message) => (
-            <div key={message._id} className={`chat ${message.sender._id === authUser._id ? "chat-end" : "chat-start"}`}> 
+            <div key={message._id} className={`chat ${message.senderId === authUser._id ? "chat-end" : "chat-start"}`}> 
                 <div className="chat-image avatar">
                     <div className="size-10 rounded-full border">
                         <img src={message.senderId === authUser._id ? authUser.profilePic || "/avatar.png" : selectedUser.profilePic} alt="profile pic" />
@@ -34,6 +34,22 @@ const ChatContainer = () => {
                     </div>
 
                 </div>
+                    <div className="chat-header mb-1">
+                      <time className="text-xs opacity-50 ml-1">
+                        {message.createdAt}
+                      </time>
+
+                    </div>
+                        <div className="chat-bubble flex">
+                        {message.image && (
+                <img
+                  src={message.image}
+                  alt="Attachment"
+                  className="sm:max-w-[200px] rounded-md mb-2"
+                />
+              )}
+              {message.text && <p>{message.text}</p>}
+                        </div>
             </div>
           ))}
       </div>
