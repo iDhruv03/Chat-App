a234f1c7d88d0caf7f70c3429afedce0918d2402
Dhruv <dhruvvv099@gmail.com>
2025-04-06 19:59:19 +0530

    added auto scroll on new message

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import {useChatStore} from "../store/useChatStore";
 import { useEffect } from 'react';
 import ChatHeader from "./ChatHeader";
@@ -13,6 +13,7 @@ import formatMessageTime from "../lib/utils";
 const ChatContainer = () => {
   const {messages, getMessages, isMessagesLoading, selectedUser, subscribeToMessages, unsubscribeFromMessages} = useChatStore();
   const {authUser}= useAuthStore();
+  const messageEndRef = useRef(null);
 
   useEffect(()=>{
     getMessages(selectedUser._id);
@@ -20,7 +21,13 @@ const ChatContainer = () => {
     subscribeToMessages();
 
     return() => unsubscribeFromMessages();
-    },[selectedUser._id, getMessages, subscribeToMessages, unsubscribeFromMessages])
+    },[selectedUser._id, getMessages, subscribeToMessages, unsubscribeFromMessages]);
+
+    useEffect(() => {
+      if(messageEndRef.current && messages){
+      messageEndRef.current.scrollIntoView({behavior: "smooth"});
+    }
+    }, [messages])
 
   if(isMessagesLoading) return (
     <div className="flex-1 flex flex-col overflow-auto">
@@ -36,7 +43,7 @@ const ChatContainer = () => {
       <ChatHeader />
       <div className="flex-1 overflow-y-auto p-4 space-y-4">
           {messages.map((message) => (
-            <div key={message._id} className={`chat ${message.senderId === authUser._id ? "chat-end" : "chat-start"}`}> 
+            <div key={message._id} className={`chat ${message.senderId === authUser._id ? "chat-end" : "chat-start"}`} ref={messageEndRef}> 
                 <div className="chat-image avatar">
                     <div className="size-10 rounded-full border">
                         <img src={message.senderId === authUser._id ? authUser.profilePic || "/avatar.png" : selectedUser.profilePic} alt="profile pic" />
