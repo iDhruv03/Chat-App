35c6f9ece8f96dcf82cfc9b19f7c6d2af6f10179
Dhruv <dhruvvv099@gmail.com>
2025-04-06 19:54:14 +0530

    imported io

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,5 +1,5 @@
 import cloudinary from "../lib/cloudinary.js";
-import { getReceiverSocketId } from "../lib/socket.js";
+import { getReceiverSocketId, io } from "../lib/socket.js";
 import Message from "../models/message.model.js";
 import User from "../models/user.model.js";
 
@@ -55,10 +55,14 @@ export const sendMessage = async(req,res) =>{
         await newMessage.save();
 
         const receiverSocketId = getReceiverSocketId(receiverId);
+        if(receiverSocketId){
+            io.to(receiverSocketId).emit("newMessage", newMessage);
+        }
         
         res.status(201).json(newMessage);
     } catch (error) {
         console.log("Error in sendMessage controller: ", error.message);
         res.status(500).json({ error: "Internal server error"});
     }
+    
 };
