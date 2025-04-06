7f328bb21f53160c8379971a6fe32881399e0256
Dhruv <dhruvvv099@gmail.com>
2025-04-06 18:16:15 +0530

    online users

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -89,7 +89,11 @@ export const useAuthStore = create((set, get) => ({
         const { authUser } = get();
         if(!authUser || get().socket?.connected) return;
 
-        const socket = io(BASE_URL);
+        const socket = io(BASE_URL, {
+            query: {
+                userId: authUser._id,
+            },
+        });
         socket.connect();
         set({ socket: socket });
     },
