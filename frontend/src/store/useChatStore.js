2fc694236c30989dc2638de34af8e4ac4c5f089d
Dhruv <dhruvvv099@gmail.com>
2025-04-06 20:14:12 +0530

    bug fix

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -55,6 +55,8 @@ export const useChatStore = create((set,get) => ({
 
 
         socket.on("newMessage", (newMessage) => {
+            if(newMessage.senderId !== selectedUser._id) return;
+            
             set({
                 messages: [...get().messages, newMessage],
             });
